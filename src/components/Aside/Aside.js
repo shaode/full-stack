7301b7ebac62c9c6d12fe1526b5c4544f18e929e
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-01-27 16:07:31 +0800

    update

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -4,71 +4,99 @@ import s from './Aside.scss';
 import Link from '../Link';
 import { Menu, Icon, Badge } from 'antd';
 
+const mainMenu = [
+  {
+    key: 'mainMenu01',
+    title: '菜单01',
+    icon: 'pie-chart',
+    subMenu:[
+      {
+        key: 'subMenu01',
+        to:'/privacy',
+        title: '子菜单01',
+        des: 'subMenu01',
+        icon: 'pie-chart',
+      },
+      {
+        key: 'subMenu02',
+        to:'/about',
+        title: '子菜单02',
+        des: 'subMenu01',
+        icon: 'pie-chart',
+      },
+      {
+        key: 'subMenu03',
+        to:'/privacy',
+        title: '子菜单03',
+        des: 'subMenu01',
+        icon: 'pie-chart',
+      },
+      {
+        key: 'subMenu04',
+        to:'/privacy',
+        title: '子菜单04',
+        des: 'subMenu01',
+        icon: 'pie-chart',
+      }
+    ]
+  },
+  {
+    key: 'mainMenu02',
+    title: '菜单02',
+    icon: 'pie-chart'
+  },
+  {
+    key: 'mainMenu03',
+    title: '菜单03',
+    icon: 'pie-chart'
+  },
+  {
+    key: 'mainMenu04',
+    title: '菜单04',
+    icon: 'pie-chart'
+  }
+]
+
 class Aside extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuCurrent: 'mainMenu01',
+      subMenuCurrent: 'subMenu01'
+    };
+  }
+
   render() {
     return (
       <aside className={s.aside + ' ' + s.clearfix}>
         <div className={s.mainMenu}>
           <div className={s.menuToggle}><span></span><span></span><span></span></div>
-          <div className={s.mainMenuItem}>
-            <Icon type="pie-chart" />
-            <div className={s.mainMenuName}>总览</div>
-          </div>
-          <div className={s.mainMenuItem}>
-            <Icon type="pie-chart" />
-            <div className={s.mainMenuName}>模块</div>
-          </div>
-          <div className={s.mainMenuItem}>
-            <Icon type="pie-chart" />
-            <div className={s.mainMenuName}>设置</div>
-          </div>
+          {
+            mainMenu.map((item, index) =>
+              <div key={item.key} className={s.mainMenuItem}>
+                <Icon type={item.icon} />
+                <div className={s.mainMenuName}>{item.title}</div>
+              </div>
+            )
+          }
         </div>
         <div className={s.subMenu}>
-          <Link to="/privacy" className={s.clearfix}>
-            <div className={s.subMenuIcon}>
-              <Icon type="laptop"/>
-            </div>
-            <div className={s.subMenuText}>
-              <div className={s.subMenuTitle}>隐私</div>
-              <div className={s.subMenuDis}>fermentum</div>
-            </div>
-          </Link>
-          <Link to="/about" className={s.clearfix}>
-            <div className={s.subMenuIcon}>
-              <Icon type="laptop"/>
-            </div>
-            <div className={s.subMenuText}>
-              <div className={s.subMenuTitle}>关于</div>
-              <div className={s.subMenuDis}>fermentum</div>
-            </div>
-          </Link>
-          <Link to="/about" className={s.clearfix}>
-            <div className={s.subMenuIcon}>
-              <Icon type="laptop"/>
-            </div>
-            <div className={s.subMenuText}>
-              <div className={s.subMenuTitle}>统计</div>
-              <div className={s.subMenuDis}>fermentum</div>
-            </div>
-          </Link>
-          <Link to="/about" className={s.clearfix}>
-            <div className={s.subMenuIcon}>
-              <Icon type="laptop"/>
-            </div>
-            <div className={s.subMenuText}>
-              <div className={s.subMenuTitle}>统计</div>
-              <div className={s.subMenuDis}>fermentum</div>
-            </div>
-          </Link>
-          <Link to="/about" className={s.clearfix}>
-            <div className={s.subMenuIcon}>
-              <Icon type="laptop"/>
-            </div>
-            <div className={s.subMenuText}>
-              <div className={s.subMenuTitle}>统计</div>
-              <div className={s.subMenuDis}>fermentum</div>
-            </div>
-          </Link>
+          {
+            mainMenu.filter((item, index) =>
+              item.key === this.state.menuCurrent
+            )[0].subMenu.map((item, index) =>
+              <Link key={item.key} to={item.to} className={s.clearfix}>
+                <div className={s.subMenuIcon}>
+                  <Icon type={item.icon}/>
+                </div>
+                <div className={s.subMenuText}>
+                  <div className={s.subMenuTitle}>{item.title}</div>
+                  <div className={s.subMenuDis}>{item.des}</div>
+                </div>
+              </Link>
+            )
+          }
         </div>
       </aside>
     );
